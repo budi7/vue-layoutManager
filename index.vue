cdcbd86dd6e453055e863ae0517347be468d3161
Budi Purnomo <budi7@users.noreply.github.com>
2021-07-31 19:24:07 +0700

    add index.vue

---
+++ index.vue
@@ -0,0 +1,39 @@
+<template functional>
+  <div v-if="!props.isLoading">
+    <div v-if="(props.isError || props.isEmpty)">
+      <div v-if="props.isError">
+        <slot name="error" />
+      </div>
+      <div v-if="props.isEmpty && !props.isError">
+        <slot name="empty" />
+      </div>
+    </div>
+    <div v-else>
+      <slot name="content" />
+    </div>
+  </div>
+  <div v-else>
+    <slot name="loader" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isError: {
+      default: false,
+      type: Boolean
+    },
+    isLoading: {
+      default: false,
+      type: Boolean
+    },
+    isEmpty: {
+      default: false,
+      type: Boolean
+    }
+  }
+}
+</script>
+
+<style />
